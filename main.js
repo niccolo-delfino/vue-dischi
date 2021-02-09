77ac1aff3aa650a96b242ce435d33278b1eee862
Niccolo Delfino <delfino.nicco@libero.it>
2021-02-09 19:11:14 +0100

    start of selection

--- main.js
+++ main.js
@@ -2,13 +2,20 @@ var app = new Vue({
   el: `#app`,
   data: {
     albums: [],
+    generi: ['All']
   },
   mounted(){
     axios
       .get('https://flynn.boolean.careers/exercises/api/array/music')
       .then((result) =>{
         this.albums = result.data.response;
-        console.log(this.albums);
+
+        this.albums.forEach((element) => {
+          if(!this.generi.includes(element.genre)){
+            this.generi.push(element.genre);
+          }
+        });
+          console.log(this.generi);
       })
   },
   methods: {
