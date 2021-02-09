438c31d90ca8fb977ce11e2ebaf81c4fbbc5ee65
Niccolo Delfino <delfino.nicco@libero.it>
2021-02-09 17:05:58 +0100

    basic js

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
 var app = new Vue({
   el: `#app`,
   data: {
+    album: [],
   },
   mounted(){
+    axios
+      .get('https://flynn.boolean.careers/exercises/api/array/music')
+      .then((result) =>{
+        this.album = result.data.response;
+      })
   },
   methods: {
   }
